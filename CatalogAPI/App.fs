f15d38e74c20d3d91452f4639d32b73a3e6d5f04
udooz <udooz@hotmail.com>
2016-06-07 15:04:42 +0530

    suave project added

---
+++ CatalogAPI/App.fs
@@ -0,0 +1,15 @@
+﻿open Suave
+open Suave.Successful
+open Retail.RetailTypes
+open Retail.RetailFn
+
+let products =
+    request (fun r ->
+        match r.queryParam "q" with
+        | Choice1Of2 q -> OK (Retail.RetailFn.searchProducts q)
+        | _ -> OK "bad")
+let webPart =
+    choose [
+        path "/" >=> OK "Ramadan Kareem"
+        path "/products" >=> products
+    ]
